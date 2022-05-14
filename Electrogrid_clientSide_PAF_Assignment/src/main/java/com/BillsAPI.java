d0de22138c0878ae8f42635d46482b43d0ef8248
dilshigeeth <dilshigeeth@gmail.com>
2022-05-14 17:11:23 +0530

    doDelete method created in API file

--- Electrogrid_clientSide_PAF_Assignment/src/main/java/com/BillsAPI.java
+++ Electrogrid_clientSide_PAF_Assignment/src/main/java/com/BillsAPI.java
@@ -68,6 +68,9 @@ public class BillsAPI extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Map paras = getParasMap(request);
+		 String output = billObj.deleteBill(paras.get("billID").toString());
+		response.getWriter().write(output); 
 	}
 	
 	// Convert request parameters to a Map
