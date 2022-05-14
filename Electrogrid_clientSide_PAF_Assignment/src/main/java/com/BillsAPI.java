bdbd57851bd4ad2d88820942369a3675c533358c
dilshigeeth <dilshigeeth@gmail.com>
2022-05-14 17:00:58 +0530

    doPost method created in API file

--- Electrogrid_clientSide_PAF_Assignment/src/main/java/com/BillsAPI.java
+++ Electrogrid_clientSide_PAF_Assignment/src/main/java/com/BillsAPI.java
@@ -1,6 +1,10 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BillsAPI")
 public class BillsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	Bill billObj = new Bill();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,7 +41,12 @@ public class BillsAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String output = billObj.insertBill(request.getParameter("billNo"),
+				 request.getParameter("billName"),
+				request.getParameter("billPrice"),
+				request.getParameter("billMonth"));
+				response.getWriter().write(output);
+		//doGet(request, response);
 	}
 
 	/**
@@ -51,5 +62,28 @@ public class BillsAPI extends HttpServlet {
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+	
+	// Convert request parameters to a Map
+			private static Map getParasMap(HttpServletRequest request)
+			{
+			 Map<String, String> map = new HashMap<String, String>();
+			try
+			 {
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			 String queryString = scanner.hasNext() ?
+			 scanner.useDelimiter("\\A").next() : "";
+			 scanner.close();
+			 String[] params = queryString.split("&");
+			 for (String param : params)
+			 { 
+				 String[] p = param.split("=");
+				 map.put(p[0], p[1]);
+				 }
+				 }
+				catch (Exception e)
+				 {
+				 }
+				return map;
+				}
 
 }
